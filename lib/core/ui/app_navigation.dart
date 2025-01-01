580d6cd4b0808df5b8291f65b56a41b4c54ce06d
kimoo112 <kimo8123008@gmail.com>
2025-01-01 17:37:33 +0200

    feat: make app navigation - floating bottom navbar make with floating_bottom_navigation_bar package

---
+++ lib/core/ui/app_navigation.dart
@@ -0,0 +1,56 @@
+import 'package:auto_car/core/utils/app_colors.dart';
+import 'package:auto_car/features/home/presentation/views/home_view.dart';
+import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+class AppNavigation extends StatefulWidget {
+  const AppNavigation({super.key});
+
+  @override
+  State<AppNavigation> createState() => _AppNavigationState();
+}
+
+class _AppNavigationState extends State<AppNavigation> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomeView(),
+    const HomeView(),
+    const HomeView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: _pages[_currentIndex],
+      bottomNavigationBar: FloatingNavbar(
+        backgroundColor: AppColors.white,
+        iconSize: 22,
+        elevation: 8,
+        itemBorderRadius: 50,
+        selectedItemColor: AppColors.primaryColor,
+        unselectedItemColor: AppColors.dark,
+        borderRadius: 30.0,
+        items: [
+          FloatingNavbarItem(
+            icon: IconlyBroken.home,
+          ),
+          FloatingNavbarItem(
+            icon: IconlyBroken.buy,
+          ),
+          FloatingNavbarItem(
+            icon: IconlyBroken.profile,
+          ),
+        ],
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
